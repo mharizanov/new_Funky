e9c6904b9e112717890dc684304f318f9e252794
mharizanov <mharizanov@gmail.com>
2013-02-25 21:24:04 +0200

    Python example

---
+++ examples/IRrecvDemo/ir.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import serial   ##sudo apt-get install python-serial
+import os
+from time import time, sleep
+
+#Set the framebuffer device to be the TFT
+os.environ["SDL_FBDEV"] = "/dev/fb1"
+
+print "Waiting for a IR command.."
+ser = serial.Serial('/dev/ttyACM0')
+ir = ser.readline()
+ir = ir.split(' ')
+print ir[2]
+if ir[2].strip() == '2602743F':
+  print "Got power off command; PID is"; print os.getpid()
+  sleep (45)
+  os.system("sudo halt -p");
+else:
+  print "got unrecognized code"
+
+ser.close()
+
