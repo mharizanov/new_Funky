282fc2c717643c501a79aff8348d7ba2cf46f04e
Martin Harizanov <mharizanov@gmail.com>
2015-01-29 16:35:44 +0200

    Adding .PNGs of board and schematic

--- examples/blink/blink.ino
+++ examples/blink/blink.ino
@@ -1,18 +1,10 @@
-
 #define LED 13
 
 void setup(){
-  Serial.begin(9600);
   pinMode(LED,OUTPUT);
-
 }
-void loop(){
-  while(1){
-  digitalWrite(LED,LOW);
-  delay(1000);
-  digitalWrite(LED,HIGH);
-  delay(1000);
-  Serial.print(".");
 
-  }
+void loop(){
+  digitalWrite(LED,!digitalRead(LED));
+  delay(1000);
 }
